20ba25f6f85d8418ab5f9ce41a81b42e1c271576
Jay Dwivedi <jdwivedi@stevens.edu>
2023-03-01 02:36:16 -0500

    OnClicked

--- app/src/main/java/com/example/mycryptoapp/presentation/coin_list/CoinListScreen.kt
+++ app/src/main/java/com/example/mycryptoapp/presentation/coin_list/CoinListScreen.kt
@@ -1,9 +1,13 @@
 package com.example.mycryptoapp.presentation.coin_list
 
+import android.util.Log
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.mycryptoapp.domain.model.Coin
@@ -14,9 +18,25 @@ fun CoinListScreen(
 ){
     val state = viewModel.coinListState.value
 
-    LazyColumn(modifier = Modifier.fillMaxHeight()) {
-        items(state.coins.size){
-            CoinListTile(coin = state.coins[it])
+    if (state.isLoading){
+        Box(modifier = Modifier.fillMaxSize() , contentAlignment = Alignment.Center){
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        }
+    }
+
+    else if (state.error != "No Error Data"){
+        Box(modifier = Modifier
+            .fillMaxSize() , contentAlignment = Alignment.Center){
+            Text(text = "Error Happened" ,)
+        }
+    }
+    else {
+        LazyColumn(modifier = Modifier.fillMaxHeight()) {
+            items(state.coins.size) {
+                CoinListTile(coin = state.coins[it]) {
+                    Log.d("CoinListScreen", it.toString())
+                }
+            }
         }
     }
 }
@@ -24,11 +44,23 @@ fun CoinListScreen(
 @Composable
 fun CoinListTile(
     coin : Coin,
+    onItemClick : (Coin) -> Unit,
 ){
     Row(modifier = Modifier
+        .clickable {
+            onItemClick(coin)
+        }
         .fillMaxWidth()
-        .height(100.dp)) {
-        Text(text = coin.name)
+        .padding(10.dp),
+    horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+        
+        Text(text = "#" + coin.rank + " " + coin.name)
+
+        
+        Text(text = coin.symbol)
+
+
 
     }
 }
